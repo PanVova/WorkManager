807cb962e65f11a2835ad0fc88b9d99a739c29ca
samurai <panvovandrik@gmail.com>
2022-01-06 18:20:58 +0200

    add controller second

--- app/src/main/java/com/panvova/conductor_example/FirstController.kt
+++ app/src/main/java/com/panvova/conductor_example/FirstController.kt
@@ -1,7 +1,21 @@
 package com.panvova.conductor_example
 
+import android.view.View
+import com.bluelinelabs.conductor.RouterTransaction
 import com.panvova.conductor_example.databinding.ControllerFirstBinding
 
 class FirstController : BaseController<ControllerFirstBinding>(ControllerFirstBinding::inflate) {
 
+    override fun onAttach(view: View) {
+        super.onAttach(view)
+
+        with(binding){
+            button2.setOnClickListener {
+                router.pushController(RouterTransaction.with(HomeController()))
+            }
+            button3.setOnClickListener {
+                router.pushController(RouterTransaction.with(SecondController()))
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/panvova/conductor_example/SecondController.kt
@@ -0,0 +1,6 @@
+package com.panvova.conductor_example
+
+import com.panvova.conductor_example.databinding.ControllerSecondBinding
+
+class SecondController : BaseController<ControllerSecondBinding>(ControllerSecondBinding::inflate) {
+}
